09498a6bdc66a39c056b0f159fb812d0b121e877
Your friendly neighborhood <120403809+LeweProjects@users.noreply.github.com>
2024-11-08 10:48:16 +0800

    testing landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client"
-import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
  const [login, setLogin] = useState(false);
 
   return (
-    <div className="flex flex-col justif">
-        Login
+    <div className="flex flex-col just">
+        
+        <p></p> Login
+        <p></p> Login
     </div>
   );
 }
